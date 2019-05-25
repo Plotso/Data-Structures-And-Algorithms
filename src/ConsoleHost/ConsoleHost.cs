d3953e2d7674427516f8290070fa18388627cf8b
Plotso <ilian_1999n@abv.bg>
2019-05-25 22:45:37 +0300

    Create solution skeleton

---
+++ src/ConsoleHost/ConsoleHost.cs
@@ -0,0 +1,12 @@
+﻿namespace ConsoleHost
+{
+    using System;
+
+    public class ConsoleHost
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("The project responsible for hosting various algorithms and playgrounds displayed on the console!");
+        }
+    }
+}
